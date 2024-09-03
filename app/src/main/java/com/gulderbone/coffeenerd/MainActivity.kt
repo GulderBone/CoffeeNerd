7c74c57331a4e8019cb7f5bc29bdebd38ca55daa
Bartkowiak <mateuszbartkowiak00@gmail.com>
2024-09-03 11:55:06 +0200

    ready

--- app/src/main/java/com/gulderbone/coffeenerd/MainActivity.kt
+++ app/src/main/java/com/gulderbone/coffeenerd/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.gulderbone.coffeenerd.intro.IntroScreenRoot
 import com.gulderbone.coffeenerd.ui.theme.CoffeeNerdTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +13,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CoffeeNerdTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                IntroScreenRoot(
+                    onSignUpClick = {},
+                    onSignInClick = {}
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CoffeeNerdTheme {
-        Greeting("Android")
     }
 }
---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.gulderbone.coffeenerd.buildlogic"
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.android.tools.common)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.ksp.gradlePlugin)
+    compileOnly(libs.room.gradlePlugin)
+    compileOnly(libs.hilt.gradlePlugin)
+}
+
+tasks {
+    validatePlugins {
+        enableStricterValidation = true
+        failOnWarning = true
+    }
+}
+
+gradlePlugin {
+    plugins {
+        register("androidApplication") {
+            id = "coffeenerd.android.application"
+            implementationClass = "AndroidApplicationConventionPlugin"
+        }
+        register("androidApplicationCompose") {
+            id = "coffeenerd.android.application.compose"
+            implementationClass = "AndroidApplicationComposeConventionPlugin"
+        }
+        register("androidLibraryCompose") {
+            id = "coffeenerd.android.library.compose"
+            implementationClass = "AndroidLibraryComposeConventionPlugin"
+        }
+        register("androidLibrary") {
+            id = "coffeenerd.android.library"
+            implementationClass = "AndroidLibraryConventionPlugin"
+        }
+        register("androidFeature") {
+            id = "coffeenerd.android.feature"
+            implementationClass = "AndroidFeatureConventionPlugin"
+        }
+        register("androidHilt") {
+            id = "coffeenerd.android.hilt"
+            implementationClass = "AndroidHiltConventionPlugin"
+        }
+    }
+}
